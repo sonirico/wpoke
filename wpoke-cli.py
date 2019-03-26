2ecc13249176e23a803203313961a42c32c8a160
Marcos <marsanben92@gmail.com>
2019-03-26 23:36:31 +0100

    Add support for some error handling

--- wpoke-cli.py
+++ wpoke-cli.py
@@ -5,15 +5,24 @@ import sys
 import uvloop
 
 from wpoke.crawlers.theme.crawler import get_theme_metadata_by_style_css
+from wpoke import exceptions as generic_exceptions
+from wpoke.crawlers.theme import exceptions as theme_exceptions
+
 
 async def check_theme(target):
-    result = await get_theme_metadata_by_style_css(target)
-    return json.dumps([theme_model.serialize() for theme_model in result],
-                      indent=4)
+    try:
+        result = await get_theme_metadata_by_style_css(target)
+    except theme_exceptions.BundledThemeException:
+        print("The site seems to be compiled by packaging tools like webpack")
+    except generic_exceptions.TargetTimeout:
+        print("The request exceeded configured time limit of %s" % str(2))
+    else:
+        return json.dumps([theme_model.serialize() for theme_model in result],
+                          indent=4)
 
 
 async def main():
-    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser = argparse.ArgumentParser(description='WordPress info gathering tool')
     parser.add_argument('--theme', dest='poke_theme',
                         action='store_const', const=check_theme,
                         required=False)
@@ -32,6 +41,7 @@ if __name__ == '__main__':
         asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
         loop = asyncio.get_event_loop()
+
         loop.run_until_complete(main())
     except KeyboardInterrupt:
         sys.exit(1)
