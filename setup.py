18b5fcaab7a79339981f39c950466c2bee60ae86
Marcos marsanben92@gmail.com <marcos.sanchez@a3sec.com>
2019-10-18 00:03:49 +0200

    release: bump 0.1.1 fix version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with io.open('requirements.txt', 'rt', encoding='utf8') as f:
 
 setup(
     name='wpoke',
-    version='0.1.0',
+    version='0.1.1',
     url='https://pypi.org/project/wpoke/',
     project_urls=OrderedDict((
         ('Code', 'https://github.com/sonirico/wpoke/'),
